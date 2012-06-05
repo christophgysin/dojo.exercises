2b50d55b894e064a1d761b9b80af0ce2318ef9c7
Christoph Gysin <christoph.gysin@ericsson.com>
2012-06-05 12:41:55 +0300

    refactor into many small functions, fix whitespace

--- src/main/java/dojo/legacy/FizzBuzz.java
+++ src/main/java/dojo/legacy/FizzBuzz.java
@@ -1,25 +1,53 @@
 package dojo.legacy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzBuzz {
-	 public String[] play(int start, int stop) {
-		 if (start > 0 && start <= stop) {
-		     int length = stop - start + 1;
-		     String[] array = new String[length];
-		     for (int i = 0; i < length; i++) {
-		         if ((start + i) % 15 == 0) {
-		                 array[i]="Fizzbuzz";
-		         } else
-		                 if ((start + i) % 5 == 0) {
-		                 array[i]="Buzz";
-		                 } else { if ((start + i) % 3 == 0)
-		                 array[i]="Fizz";
-		         else {
-		                 array[i] =  "" + (start + i);
-		         }}
-		     }
-		     return array;
-		 } else {
-		     throw new IllegalArgumentException();
-		 }
-	 }
+
+    public String[] play(int start, int stop) {
+
+        if (argumentsValid(start, stop)) {
+            List<String> values = calculateValues(start, stop);
+            return values.toArray(new String[values.size()]);
+        } else {
+            throw new IllegalArgumentException();
+        }
+     }
+
+    private List<String> calculateValues(int start, int stop) {
+        
+        int length = stop - start + 1;
+        List<String> values = new ArrayList<String>(length);
+
+        for (int value = start; value <= stop; value++) {
+
+            if (isFizzBuzz(value)) {
+                values.add("Fizzbuzz");
+            } else if (isFizz(value)) {
+                values.add("Fizz");
+            } else if (isBuzz(value)) {
+                values.add("Buzz");
+            } else {
+                values.add(Integer.toString(value));
+            }
+        }
+        return values;
+    }
+
+    private boolean isFizz(int value) {
+        return value % 3 == 0;
+    }
+
+    private boolean isBuzz(int value) {
+        return value % 5 == 0;
+    }
+
+    private boolean isFizzBuzz(int value) {
+        return isFizz(value) && isBuzz(value);
+    }
+
+    private boolean argumentsValid(int start, int stop) {
+        return start > 0 && start < stop;
+    }
 }
