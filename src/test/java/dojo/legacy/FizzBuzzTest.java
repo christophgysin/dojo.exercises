1fdf0a2f3756176228548588c099da754cbe0a44
Christoph Gysin <christoph.gysin@ericsson.com>
2012-06-05 10:46:53 +0300

    implement zsolts github comments
    * use setUp() to create FizzBuzz instance
    * use expect= to handle exceptions
    * rename test cases

--- src/test/java/dojo/legacy/FizzBuzzTest.java
+++ src/test/java/dojo/legacy/FizzBuzzTest.java
@@ -1,78 +1,79 @@
 package dojo.legacy;
 
 import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.fail;
 
 
 public class FizzBuzzTest {
-	
-	@Test
-	public void testZero()
-	{
-		try {
-			new FizzBuzz().play(0, 1);
-			fail();
-		} catch(IllegalArgumentException e) {
-			/* success(); */
-		} catch(Exception e) {
-			fail("wrong exception thrown:" + e.getClass().toString());
-		}
-	}
-	@Test
-	public void testNegative()
-	{
-		try {
-			new FizzBuzz().play(-1, 1);
-			fail();
-		} catch(IllegalArgumentException e) {
-			/* success(); */
-		} catch(Exception e) {
-			fail("wrong exception thrown:" + e.getClass().toString());
-		}
-	}
-	@Test
-	public void testFirstThree()
-	{
-		assertArrayEquals(new FizzBuzz().play(1, 3),
-				new String[]{
-					"1",
-					"2",
-					"Fizz"
-					});
-	}
-	@Test
-	public void testFirstFive()
-	{
-		assertArrayEquals(new FizzBuzz().play(1, 5),
-				new String[]{
-					"1",
-					"2",
-					"Fizz",
-					"4",
-					"Buzz"
-					});
-	}
-	@Test
-	public void testFirstFifteen()
-	{
-		assertArrayEquals(new FizzBuzz().play(1, 15),
-				new String[]{
-					"1",
-					"2",
-					"Fizz",
-					"4",
-					"Buzz",
-					"Fizz",
-					"7",
-					"8",
-					"Fizz",
-					"Buzz",
-					"11",
-					"Fizz",
-					"13",
-					"14",
-					"Fizzbuzz"
-					});
-	}
+
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp()
+    {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testStartIsZero()
+    {
+        fizzBuzz.play(0, 1);
+        fail();
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testStopIsSmallerThanStart()
+    {
+        fizzBuzz.play(1, 0);
+        fail();
+    }
+
+    @Test
+    public void testFirstThree()
+    {
+        assertArrayEquals(fizzBuzz.play(1, 3),
+                new String[]{
+                    "1",
+                    "2",
+                    "Fizz"
+                    });
+    }
+
+    @Test
+    public void testFirstFive()
+    {
+        assertArrayEquals(fizzBuzz.play(1, 5),
+                new String[]{
+                    "1",
+                    "2",
+                    "Fizz",
+                    "4",
+                    "Buzz"
+                    });
+    }
+
+    @Test
+    public void testFirstFifteen()
+    {
+        assertArrayEquals(fizzBuzz.play(1, 15),
+                new String[]{
+                    "1",
+                    "2",
+                    "Fizz",
+                    "4",
+                    "Buzz",
+                    "Fizz",
+                    "7",
+                    "8",
+                    "Fizz",
+                    "Buzz",
+                    "11",
+                    "Fizz",
+                    "13",
+                    "14",
+                    "Fizzbuzz"
+                    });
+    }
 }
